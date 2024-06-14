0f894b03ce34f0198b1ddd23bb9c74f2b354e6b3
xiangjincheng <xjcee@outlook.com>
2024-06-15 03:10:33 +0800

    init workspace

---
+++ build/unitree_legged_msgs/CMakeFiles/unitree_legged_msgs_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/BmsCmd.lisp"
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/BmsState.lisp"
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/Cartesian.lisp"
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/HighCmd.lisp"
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/HighState.lisp"
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/IMU.lisp"
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/LED.lisp"
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/LowCmd.lisp"
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/LowState.lisp"
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/MotorCmd.lisp"
+  "/home/cheng/unitree_noetic/devel/share/common-lisp/ros/unitree_legged_msgs/msg/MotorState.lisp"
+  "CMakeFiles/unitree_legged_msgs_generate_messages_lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/unitree_legged_msgs_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
